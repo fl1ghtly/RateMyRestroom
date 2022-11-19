c8908cf95d4dd0cd5a3d61f5e2f9305e5127dc32
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2022-11-19 13:38:47 -0800

    migrate to restroom

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose  = require("mongoose");
 const { ejs } = require("ejs");
+const { Double } = require("mongodb");
 require('dotenv').config();
 
 const app = express();
@@ -8,17 +9,17 @@ app.set('view engine', 'ejs');
 
 mongoose.connect(process.env.MONGO_URI);
 
-const testSchema = new mongoose.Schema({
-  name: String,
-  hasRings: Boolean
+const restroomSchema = new mongoose.Schema({
+  rating: Double,
+  review: String
 });
 
-const Test = mongoose.model('Planet', testSchema);
+const Restroom = mongoose.model('Restroom', restroomSchema);
 
 app.get('/', (req, res) => {
-    Test.find({}, function(err, tests) {
+    Restroom.find({}, function(err, restrooms) {
       res.render('index', {
-          testList: tests
+          restList: restrooms
       });
 
     })
@@ -27,22 +28,3 @@ app.get('/', (req, res) => {
 app.listen(4000, function() {
     console.log("Server is running");
 })
-/*
-async function getData(db, collection) {
-  const client = new MongoClient(process.env.MONGO_URI);
-  try {
-    await client.connect();
-    // database and collection code goes here
-    const db = client.db(db);
-    const coll = db.collection(collection);
-    // find code goes here
-    const cursor = coll.find();
-    // iterate code goes here
-    await cursor.forEach(console.log);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-*/
-//getData("owlhacks2022", "restroom").catch(console.dir);
